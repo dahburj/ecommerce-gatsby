8c826eeb0760ebd11a5bc2739f77a513e6d391ef
Jamal Dahbur <dahbur.jamal@gmail.com>
2020-01-03 19:37:05 -0600

    added hotjar

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-hotjar-tracking`,
+      options: {
+        includeInDevelopment: false,
+        id: 1630417,
+        sv: 6
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
